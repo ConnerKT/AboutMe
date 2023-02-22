26b831deb48976ee554943a2352dc9e88f45a7b7
ConnerKT <connerthompson121@gmail.com>
2023-02-22 00:55:07 -0600

    Create README.md

---
+++ script.js
@@ -0,0 +1,80 @@
+// While true, ask name, if not break
+// console.log("Starting while statement")
+let username;
+while (true) {
+let username = prompt ("What is your name?");
+
+
+    if (username === "") {
+        alert("Name must be inputed!");
+    }else {
+    alert ("Hello " + username + "!");
+        break;
+    }
+}
+
+let x = 0;
+
+// console.log("Finishing while")
+
+function clickme(){
+    "use strict";
+    let origin = prompt("I grew up in Memphis!");
+    let originlow = origin.toLowerCase();
+    if (originlow === "y" ||originlow === "yes" ){
+        alert("Correct!");
+        x++;
+        console.log(x)
+        // console.log("correct!")
+    }else {
+        alert("Incorrect.. :(");
+    }
+    let age = prompt("I'm 19 years old!");
+    let agelow = age.toLowerCase();
+    if (agelow === "n" || agelow === "no" ){
+        alert("Correct!");
+        x++;
+        //console.log(x)
+        // console.log("correct!")
+    }else {
+        alert("Incorrect.. :(");
+    // console.log(agelow)
+    }
+    let experience = prompt("I started programming as a kid!");
+    let experiencelow = experience.toLowerCase();
+    if (experiencelow === "n" || experiencelow === "no" ){
+        alert("Correct!");
+        x++;
+        //console.log(x)
+        // console.log("correct!")
+    }else {
+        alert("Incorrect.. :(");
+    }
+    let goal = prompt("Was money one of my goals?");
+    let goallow = goal.toLowerCase();
+    if (goallow === "n" || goallow === "no"){
+        alert("Correct!");
+        x++;
+        //console.log(x)
+        // console.log("correct!")
+    }else {
+        alert("Incorrect.. :(");
+    }
+    let job = prompt("Did I work at a Tech firm?");
+    let joblow = job.toLowerCase();
+    if (joblow === "n" || joblow === "no" ){
+        alert("Correct!");
+        x++;
+        //console.log(x)
+        // console.log("correct!")
+    }else {
+        alert("Incorrect.. :(");
+ 
+    }
+    //console.log(x)
+    //Its not the following for this alert function not working:
+    //X variable, Function is being called, Function doesn't need to be recalled, The number goes up in console, the syntax is correct
+    // username was the issue, not declared outside of while loop, so it didn't exist
+    alert("Congrats" + " " + username + " " + "you made a" + " "+ x + " " + "out of 5!");   
+}
+
